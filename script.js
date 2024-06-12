8bb12d164cfc7c5717cca9520d76dbe638e23b9f
frankMarcelino <frank.silva.ti@gmail.com>
2024-06-12 10:10:24 -0300

    implemntando logica de escolha para o comentario

--- script.js
+++ script.js
@@ -1,49 +1,47 @@
-const nomesAdicionados = [];
+const nomesAdicionadosLeitor = [];
+const nomesAdicionadosComentario = [];
 function adicionarNome() {
   
   const inputNome = document.getElementById('nome');
   const nome = inputNome.value.trim(); // Remove espaços em branco no início e no fim
   if (nome) {
-    const listaNomes = document.getElementById('listaNomes');
-    const novoItem = document.createElement('li');
-    novoItem.textContent = nome;
+    const listaNomesLeitor = document.getElementById('listaNomesLeitor');
+    const listaNomesComentario = document.getElementById('listaNomesComentario');
+    const novoItemLeitor = document.createElement('li');
+    const novoItemComentario = document.createElement('li');
+    novoItemLeitor.textContent = nome;
+    novoItemComentario.textContent = nome;
 
-    // Atribuir o valor do nome como ID
-    novoItem.id = nome;
-    novoItem.className = 'leitores';
+    // Atribuir o valor do nome como ID e class
+    novoItemLeitor.id = nome;
+    novoItemComentario.id = nome;
+    novoItemLeitor.className = 'leitores';
+    novoItemComentario.className = 'comentarios';
 
-    listaNomes.appendChild(novoItem);
-    nomesAdicionados.push(nome);
+    listaNomesLeitor.appendChild(novoItemLeitor);
+    listaNomesComentario.appendChild(novoItemComentario);
+    nomesAdicionadosLeitor.push(nome);
+    nomesAdicionadosComentario.push(nome);
 
     // Limpar o campo de entrada
     inputNome.value = '';
   }
-  console.log(nomesAdicionados)
 }
-let nomesSorteados = [...nomesAdicionados]; // Cria uma cópia do array original
+let nomesSorteadosLeitor = [...nomesAdicionadosLeitor]; // Cria uma cópia do array original
+let nomesSorteadosComentarios = [...nomesAdicionadosComentario]; // Cria uma cópia do array original
 
 // Função para sortear um nome aleatório e garantir que não seja repetido
-function sortearNome() {
-    if (nomesSorteados.length === 0) {
+function sortearNomeLeitor() {
+    if (nomesSorteadosLeitor.length === 0) {
        reiniciarLista();  
     }
-
-    const indiceSorteado = Math.floor(Math.random() * nomesSorteados.length);
-    const nomeSorteado = nomesSorteados.splice(indiceSorteado, 1)[0];
+    const indiceSorteado = Math.floor(Math.random() * nomesSorteadosLeitor.length);
+    const nomeSorteado = nomesSorteadosLeitor.splice(indiceSorteado, 1)[0];
     return nomeSorteado;
 }
 
-
-function reiniciarLista() {
-  nomesSorteados = [...nomesAdicionados];
-  const corLeitores = document.getElementsByClassName('leitores');
-  for (const elemento of corLeitores) {
-    elemento.style.color = 'white';
-  }
-}
-
-function exibirNomeSorteado() {
-  const nome = sortearNome();
+function exibirNomeSorteadoLeitor() {
+  const nome = sortearNomeLeitor();
   const elementoNomeSorteado = document.getElementById(nome);
 
   if (elementoNomeSorteado) {
@@ -53,4 +51,32 @@ function exibirNomeSorteado() {
   } else {
     alert("Nome não encontrado: " + nome);
   }
+}
+
+function sortearNomeComentario() {
+  if (nomesSorteadosComentarios.length === 0) {
+     reiniciarLista();  
+  }
+  const indiceSorteado = Math.floor(Math.random() * nomesSorteadosComentarios.length);
+  const nomeSorteado = nomesSorteadosComentarios.splice(indiceSorteado, 1)[0];
+  return nomeSorteado;
+}
+
+
+function exibirNomeSorteadoComentario() {
+  const nome = sortearNomeComentario();
+  const elementoNomeSorteado = document.getElementById(nome);
+
+  if (elementoNomeSorteado) {
+    // Altere a cor do elemento (por exemplo, para vermelho)
+    elementoNomeSorteado.style.color = 'green';
+    alert("Nome sorteado: " + nome);
+  } else {
+    alert("Nome não encontrado: " + nome);
+  }
+}
+
+function reiniciarLista() {
+  nomesSorteadosLeitor = [...nomesAdicionadosLeitor];
+  nomesSorteadosComentarios = [...nomesAdicionadosComentario];
 }
